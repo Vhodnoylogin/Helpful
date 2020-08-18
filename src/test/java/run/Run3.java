8712f78337f0bf88f8c47ebe8b7ed3ba12723d1a
Радимир Кармацкий <kill.spama@gmail.com>
2020-08-18 17:04:04 +0300

    commit 4 add throws exceptions to all methods

--- src/test/java/run/Run3.java
+++ src/test/java/run/Run3.java
@@ -2,9 +2,5 @@ package run;
 
 public class Run3 {
     public static void main(String[] args) {
-//        List<Integer> qql = StreamDataCollection.newList();
-//        ArrayList<Integer> qql1 = StreamDataCollection.newList1().;
-//        StreamDataCollection.<Integer,ArrayList<Integer>>newList1().;
-//        //List<Integer> qql2 = StreamDataCollection.newList1();
     }
 }
